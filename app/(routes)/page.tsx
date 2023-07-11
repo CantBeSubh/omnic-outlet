de4589312d94b84c1c4f5727d39c2e958ff4adcf
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-07-11 10:35:10 +0530

    Navbar

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,13 @@
+interface HomePageProps {
+
+}
+
+const HomePage: React.FC<HomePageProps> = () => {
+    return (
+        <>
+            Hello Store
+        </>
+    );
+}
+
+export default HomePage;
